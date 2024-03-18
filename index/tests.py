6492cd9e3fea2e5b03f88cfe2c7ce6d6cb98a5f7
sav21age <sav21age@gmail.com>
2024-03-18 14:45:52 +0300

    Add index page test

--- index/tests.py
+++ index/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexTest(TestCase):
+    fixtures = ['fixtures/db.json', ]
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        """ Test index """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
