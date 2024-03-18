2177744622ed528ebc3dc5be240f832022d6b11b
sav21age <sav21age@gmail.com>
2024-03-18 18:11:48 +0300

    Remove tertiary colors

--- profiles/allauth/forms.py
+++ profiles/allauth/forms.py
@@ -45,7 +45,8 @@ class CustomLoginForm(LoginForm):
         for field in self.fields:
             if field == 'remember':
                 self.fields[field].widget.attrs.update({
-                    'style': 'accent-color: #557F27; width: 15px; height: 15px;',
+                    # 'style': 'accent-color: #557F27; width: 15px; height: 15px;',
+                    'style': 'width: 15px; height: 15px;',
                     'checked': True,
                 })
             else:
