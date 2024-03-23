14a6fb7527c7c5e313cfb47f1283203f44284c0c
sav21age <sav21age@gmail.com>
2024-03-23 15:45:48 +0300

    Add back to top button

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -84,6 +84,22 @@ let xhr = new XMLHttpRequest();
   });
 
   /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
    * Animation on scroll
    */
   // AOS.init({
