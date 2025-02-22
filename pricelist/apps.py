36ae1db61e71407f9745bf750799be8ed0ab5354
sav21age <sav21age@gmail.com>
2025-02-22 10:38:19 +0300

    Change apps order in admin

--- pricelist/apps.py
+++ pricelist/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class PricelistConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pricelist'
-    verbose_name = '37. Прайс-лист'
+    verbose_name = '46. Прайс-лист'
--- roses/apps.py
+++ roses/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class RosesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'roses'
-    verbose_name = '13. Каталог роз'
+    verbose_name = '19. Каталог роз'
 
     def ready(self):
         import common.signals
