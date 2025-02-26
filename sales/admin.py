8cb5d7ced896a41f25abdaef9656135d5bc13a46
sav21age <sav21age@gmail.com>
2025-02-26 20:15:20 +0300

    Add min_num=1 to promotions in sales

--- sales/admin.py
+++ sales/admin.py
@@ -17,7 +17,7 @@ class PromotionItemInline(admin.StackedInline):
     extra = 0
     show_change_link = True
     formfield_overrides = formfield_overrides
-
+    min_num = 1
 
 @admin.register(Promotion)
 class PromotionAdmin(SortableAdminBase, admin.ModelAdmin):
