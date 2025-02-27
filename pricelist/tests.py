fc0b0190178f38a46124104b841cd570ed2d0aad
sav21age <sav21age@gmail.com>
2025-02-27 17:03:17 +0300

    Add test to pricelist

--- pricelist/tests.py
+++ pricelist/tests.py
@@ -2,13 +2,23 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 
-class PriceListTest(TestCase):
+class PriceDetailTest(TestCase):
     fixtures = ['fixtures/db.json', ]
 
     def setUp(self):
         self.client = Client()
 
     def test_detailview(self):
-        """ Test ListView """
+        """ Test DetailView """
         response = self.client.get(reverse('price_list_detail'))
         self.assertEqual(response.status_code, 200)
+
+
+class PriceDetailTestNotExists(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_detailview(self):
+        """ Test DetailView """
+        response = self.client.get(reverse('price_list_detail'))
+        self.assertEqual(response.status_code, 404)
